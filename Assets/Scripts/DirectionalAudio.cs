654e28c7757bc9f6ccb3b8d172f6cd7824d251ab
Clay Ewing <c.ewing@miami.edu>
2018-08-31 16:40:16 -0400

    Adding directional audio prefab

---
+++ Assets/Scripts/DirectionalAudio.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DirectionalAudio : MonoBehaviour {
+    public GameObject player;
+    public float raycastDistance;
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    // Update is called once per frame
+    void Update() {
+
+        RaycastHit hit;
+        Debug.DrawRay(transform.position, transform.forward * raycastDistance, Color.green);
+        if (Physics.Raycast(transform.position, transform.forward * raycastDistance, out hit))
+        {
+           if(hit.transform.name == "Listener")
+            {
+                if(!GetComponent<AudioSource>().isPlaying)
+                {
+                    GetComponent<AudioSource>().Play();
+                }
+            }
+
+        }
+
+
+    }
+}
