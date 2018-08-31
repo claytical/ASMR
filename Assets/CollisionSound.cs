16c6dffcad57437af2c0b9f752149b566947f266
Clay Ewing <c.ewing@miami.edu>
2018-08-31 14:43:57 -0400

    add dripping prefab
    in main scene, new prefabs for experiences have been created. Podium experience has a single looping sound. Dripping experience has a variable speed object emitter that plays a sound on contact.

---
+++ Assets/CollisionSound.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollisionSound : MonoBehaviour {
+    private bool playedAudio;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(playedAudio)
+        {
+            if(!GetComponent<AudioSource>().isPlaying)
+            {
+                Destroy(gameObject);
+            }
+        }
+	}
+    void OnCollisionEnter(Collision collision)
+    {
+        GetComponent<AudioSource>().Play();
+        playedAudio = true;
+    }
+
+    }
---
+++ Assets/Scripts/NoVRNavigation.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NoVRNavigation : MonoBehaviour {
+    public float speed;
+    private Rigidbody body;
+	// Use this for initialization
+	void Start () {
+        body = GetComponent<Rigidbody>();
+        body.freezeRotation = true;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        checkInput();	
+	}
+
+    void checkInput()
+    {
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            body.MovePosition(transform.position + (Vector3.forward * speed));
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            body.MovePosition(transform.position + (Vector3.back * speed));
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            body.MovePosition(transform.position + (Vector3.right * speed));
+        }
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            body.MovePosition(transform.position + (Vector3.left * speed));
+        }
+
+    }
+}
