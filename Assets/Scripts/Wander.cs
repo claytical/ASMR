099b1de27b894f67d42c29f7c9e56e6e88c4c4c8
Clay Ewing <c.ewing@miami.edu>
2018-11-07 15:57:33 -0500

    add nav mesh and new dome

--- Assets/Scripts/Wander.cs
+++ Assets/Scripts/Wander.cs
@@ -3,60 +3,38 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Wander : MonoBehaviour {
-    public float speed;
-    public List<Transform> points;
-    public bool halfPipeMotion;
+    public Transform[] points;
+    private int destinationPoint = 0;
+    private UnityEngine.AI.NavMeshAgent agent;
+    /*    public bool halfPipeMotion;
     private int currentPoint = 0;
     private int direction = 1;
-
+    */
     // Use this for initialization
     void Start () {
-        if (points.Count > 0)
-        {
-            GameObject g = new GameObject();
-            g.transform.position = gameObject.transform.localPosition;
-            points.Add(g.transform);
-            g.transform.parent = points[0].gameObject.transform.parent;
 
-        }
+        agent = GetComponent<UnityEngine.AI.NavMeshAgent>();
+        agent.autoBraking = false;
+        GotoNextPoint();
     }
-	
-	// Update is called once per frame
-	void Update () {
-        if (points.Count > 1)
-        {
-            transform.position = Vector3.MoveTowards(transform.position, points[currentPoint].position, speed);
-            if (halfPipeMotion)
-            {
 
-                if (transform.position == points[currentPoint].position)
-                {
-                    if (currentPoint == points.Count - 1)
-                    {
-                        direction = -1;
-                    }
-                    if (currentPoint == 0)
-                    {
-                        direction = 1;
-                    }
-                    currentPoint += direction;
-                }
+    void GotoNextPoint()
+    {
+        // Returns if no points have been set up
+        if (points.Length == 0)
+            return;
 
+        // Set the agent to go to the currently selected destination.
+        agent.destination = points[destinationPoint].position;
 
-            }
-            else
-            {
-
-                if (transform.position == points[currentPoint].position)
-                {
-                    currentPoint++;
-                    if (currentPoint == points.Count)
-                    {
-                        currentPoint = 0;
-                    }
-                }
-            }
+        // Choose the next point in the array as the destination,
+        // cycling to the start if necessary.
+        destinationPoint = (destinationPoint + 1) % points.Length;
+    }
+    // Update is called once per frame
+    void Update() {
+        if (!agent.pathPending && agent.remainingDistance < 0.5f) {
+            GotoNextPoint();
         }
- 
     }
 }
