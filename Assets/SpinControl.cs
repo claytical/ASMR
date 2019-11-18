629e98c84ed67e92426ab48b04a5321ea3323581
Clay Ewing <c.ewing@miami.edu>
2019-11-18 14:41:58 -0500

    updates to objects

---
+++ Assets/SpinControl.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpinControl : MonoBehaviour
+{
+
+    public GameObject objectToSpin;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+ //       objectToSpin.transform.RotateAround(transform.position, Time.deltaTime);
+        objectToSpin.transform.Rotate(50 * Time.deltaTime, 0, 0, Space.World); //rotates 50 degrees per second around z axis
+    }
+}
