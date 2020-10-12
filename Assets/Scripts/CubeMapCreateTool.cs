29493a373da08408210b5a412ba7c8e1526061ae
XueyanZh <54363064+XueyanZh@users.noreply.github.com>
2020-10-11 23:06:21 -0400

    assets&scene UPDATE

---
+++ Assets/Scripts/CubeMapCreateTool.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+public class CubeMapCreateTool : ScriptableWizard
+{
+    public Transform renderPosition;
+    public Cubemap customCubemap;
+
+    private void OnWizardUpdate()
+    {
+        helpString = "Select transform to render" + "from and cubemap to render into";
+        if (renderPosition!=null&&customCubemap!=null)
+        {
+            isValid = true;
+        }
+        else
+        {
+            isValid = false;
+        }
+    }
+    void OnWizardCreate()
+    {
+        GameObject go = new GameObject("CubeCam", typeof(Camera));
+
+        go.transform.position = renderPosition.position;
+        go.transform.rotation = Quaternion.identity;
+
+        go.GetComponent<Camera>().RenderToCubemap(customCubemap);
+
+        DestroyImmediate(go);
+    }
+    [MenuItem("Custom/Render Cubemap")]
+        static void RenderCubemap()
+    {
+        ScriptableWizard.DisplayWizard("Render CubeMap", typeof(CubeMapCreateTool), "Render!");
+    }
+}
